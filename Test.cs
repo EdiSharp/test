7405ef31174badf0cf4c4ebba7fc7531f1ad4ebe
EdiSharp <greatedison7@gmail.com>
2025-11-10 00:10:41 +0400

    third commit. Just testing

--- Test.cs
+++ Test.cs
@@ -15,7 +15,9 @@ else
 accessAllowed = storedPassword == enteredPassword ? true : false;
 
 Console.WriteLine(accessAllowed);
-Console.ReadLine(); 
+Console.ReadLine();
 
 int x = 5; // just a comment to test git changes
+
+string text = "Hello, World!"; 
 
